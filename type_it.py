6b6867d0d34c74fa7aa35a2e39e2945f2ef88a5e
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-10-17 16:46:55 +0700

    Update type_it.py

--- type_it.py
+++ type_it.py
@@ -1,13 +1,10 @@
 import pyautogui
 import time
 
-# Tunggu beberapa detik sebelum mulai mengetik
 time.sleep(5)
 
-# Baca teks dari output.txt
 with open('output.txt', 'r') as file:
     text = file.read()
 
-# Ketik teks secara otomatis
 pyautogui.typewrite(text, interval=0.05)
 
